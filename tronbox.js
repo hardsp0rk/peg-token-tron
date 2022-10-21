715518d1ffa880e25e216bed684448e035116391
hardsp0rk <107102839+hardsp0rk@users.noreply.github.com>
2022-10-21 10:30:51 +0800

    Initialize tronbox project

---
+++ tronbox.js
@@ -0,0 +1,40 @@
+const port = process.env.HOST_PORT || 9090
+
+module.exports = {
+  networks: {
+    mainnet: {
+      privateKey: process.env.PRIVATE_KEY_MAINNET,
+      userFeePercentage: 100,
+      feeLimit: 1000 * 1e6,
+      fullHost: 'https://api.trongrid.io',
+      network_id: '1'
+    },
+    shasta: {
+      privateKey: process.env.PRIVATE_KEY_SHASTA,
+      userFeePercentage: 100,
+      feeLimit: 1000 * 1e6,
+      fullHost: 'https://api.shasta.trongrid.io',
+      network_id: '2'
+    },
+    nile: {
+      privateKey: process.env.PRIVATE_KEY_NILE,
+      userFeePercentage: 100,
+      feeLimit: 1000 * 1e6,
+      fullHost: 'https://api.nileex.io',
+      network_id: '3'
+    },
+    development: {
+      // For trontools/quickstart docker image
+      privateKey: process.env.PRIVATE_KEY_DEVELOPMENT,
+      userFeePercentage: 0,
+      feeLimit: 1000 * 1e6,
+      fullHost: 'http://127.0.0.1:' + port,
+      network_id: '9'
+    },
+    compilers: {
+      solc: {
+        version: '0.5.4'
+      }
+    }
+  }
+}
