b818876407abb546fd95f48ed88e9e83deba2842
hardsp0rk <107102839+hardsp0rk@users.noreply.github.com>
2022-10-21 14:38:03 +0800

    Add deploy scripts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,16 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var PegToken = artifacts.require("./pegToken/PegToken.sol");
+var ProxyAdmin = artifacts.require("./proxy/ProxyAdmin.sol");
+var TransparentUpgradeableProxy = artifacts.require("./proxy/TransparentUpgradeableProxy.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin, 10000);
+module.exports = async function(deployer) {
+  await deployer.deploy(PegToken);
+  await deployer.deploy(ProxyAdmin);
+  await deployer.deploy(
+    TransparentUpgradeableProxy,
+    PegToken.address,
+    ProxyAdmin.address,
+    "0x4cd88b7600000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000000a4255534420546f6b656e0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044255534400000000000000000000000000000000000000000000000000000000"
+  );
+  const impl = await PegToken.deployed();
+  await impl.initialize("BUSD Token", "BUSD");
 };
